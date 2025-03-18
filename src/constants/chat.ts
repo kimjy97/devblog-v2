76f8868918ffbff99deb496b46423c519e4c4f94
JongYeon <99719878+kimjy97@users.noreply.github.com>
2025-03-18 22:28:37 +0900

    feat: gemini model 업데이트 (1.5 flash -> 2.0 flash exp)

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -5,7 +5,7 @@ export const chatMenuWidth = '320px';
 export const contentsWidth = '60%';
 
 export const modelArr: any[] = [
-  { model: 'gemini-1.5-flash-002', name: 'Gemini', type: 'gemini', maxToken: 40000 },
+  { model: 'gemini-2.0-flash-exp', name: 'Gemini', type: 'gemini', maxToken: 40000 },
   { model: 'gpt-3.5-turbo', name: 'ChatGPT', type: 'gpt', maxToken: 10000 },
   { model: 'llama-3-8b-it-ko-chang-Q8:latest', name: 'Tom', type: 'ollama', maxToken: 40000 },
 ];
